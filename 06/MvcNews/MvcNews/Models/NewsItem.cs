a121685d50fd8ca914a39054f18c3b5d5a70cb31
monksbronks <monika.sztuder@student.put.poznan.pl>
2024-04-19 12:37:05 +0200

    lab 06 - mvcnews

---
+++ 06/MvcNews/MvcNews/Models/NewsItem.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+
+
+namespace MvcNews.Models
+{
+    public class NewsItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime TimeStamp { get; set; }
+        [Required(ErrorMessage = "Pole Tekst jest wymagane.")]
+        [StringLength(140, MinimumLength = 5, ErrorMessage = "Długość Tekstu musi być od 5 do 140 znaków.")]
+        public string Text { get; set; } = string.Empty;
+        
+        [Timestamp]
+        public byte[]? RowVersion { get; set; }
+
+
+    }
+}
