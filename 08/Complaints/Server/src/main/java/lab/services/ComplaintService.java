f959274f0d7f4617d7d4dc35d9abdb2ae9e7f1e6
monksbronks <monika.sztuder@student.put.poznan.pl>
2024-05-04 09:14:04 +0200

    lab08 - cw2 - ModelMapper

--- 08/Complaints/Server/src/main/java/lab/services/ComplaintService.java
+++ 08/Complaints/Server/src/main/java/lab/services/ComplaintService.java
@@ -15,32 +15,34 @@ import jakarta.transaction.Transactional;
 public class ComplaintService {
     @Inject
     private ComplaintRepository repository;
+    @Inject
+    private ModelMapper mapper;
     @Transactional
     public void create(ComplaintDTO dto) {
-        ModelMapper mapper = new ModelMapper();
+//        ModelMapper mapper = new ModelMapper();
         repository.create(mapper.map(dto, Complaint.class));
     }
     @Transactional
     public void edit(ComplaintDTO dto) {
-        ModelMapper mapper = new ModelMapper();
+//        ModelMapper mapper = new ModelMapper();
         repository.edit(mapper.map(dto, Complaint.class));
     }
 
     @Transactional
     public void remove(ComplaintDTO dto) {
-        ModelMapper mapper = new ModelMapper();
+//        ModelMapper mapper = new ModelMapper();
         repository.remove(mapper.map(dto, Complaint.class));
     }
 
     @Transactional
     public ComplaintDTO find(Long id) {
-        ModelMapper mapper = new ModelMapper();
+//        ModelMapper mapper = new ModelMapper();
         Complaint complaint = repository.find(id);
         return mapper.map(complaint, ComplaintDTO.class);
     }
     @Transactional
     public List<ComplaintDTO> findAll(String status) {
-        ModelMapper mapper = new ModelMapper();
+//        ModelMapper mapper = new ModelMapper();
         List<Complaint> entityList = repository.findAll(status);
         Type listType = new TypeToken<List<ComplaintDTO>>() {}.getType();
         List<ComplaintDTO> dtoList = mapper.map(entityList, listType);
